a96ae76a0785516217f2b6e8e63ed9b29123963b
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-05-30 18:32:01 +0900

    5/30 UJH

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean inFirstInput = true;  // 입력 값이 처음 입력되는가를 체크
+    boolean isFirstInput = true;  // 입력 값이 처음 입력되는가를 체크
     int resultNumber = 0; // 계산된 결과 값을 저장하는 변수
     char operator = '+'; // 입력된 연산자를 저장하는 변수
 
@@ -22,10 +24,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClick(View view) {
-        if (view.getId() == R.id.number_one_button){
-            resultText.setText("1");
-        }else {
+        Button getButton = findViewById(view.getId());
 
+        switch (view.getId()) {
+            case R.id.all_clear_button:
+                isFirstInput = true;
+                resultNumber = 0;
+                operator = '+';
+                resultText.setTextColor(0xFFB8B7B7);
+                resultText.setText(resultNumber + "");
+                break;
+
+            case R.id.number_zero_button:
+            case R.id.number_one_button:
+            case R.id.number_two_button:
+            case R.id.number_three_button:
+            case R.id.number_four_button:
+            case R.id.number_five_button:
+            case R.id.number_six_button:
+            case R.id.number_seven_button:
+            case R.id.number_eight_button:
+            case R.id.number_nine_button:
+
+                if (isFirstInput) {
+                    resultText.setTextColor(0xFF000000);
+                    resultText.setText(getButton.getText().toString());
+                    isFirstInput = false;
+                } else {
+                    resultText.append(getButton.getText().toString());
+                }
+                break;
+
+            default:
+                Toast.makeText(getApplicationContext(), getButton.getText().toString() + "버튼이클릭되었습니다.", Toast.LENGTH_LONG).show();
+                break;
         }
     }
 }
