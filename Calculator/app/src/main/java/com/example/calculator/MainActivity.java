866866b8583b278336c6bc2147b62b09cd2abf06
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-05-30 16:33:03 +0900

    5/30 test

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean inFirstInput = true;  // 입력 값이 처음 입력되는가를 체크
+    int resultNumber = 0; // 계산된 결과 값을 저장하는 변수
+    char operator = '+'; // 입력된 연산자를 저장하는 변수
+
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultText = findViewById(R.id.result_text);
+    }
+
+    public void buttonClick(View view) {
+        if (view.getId() == R.id.number_one_button){
+            resultText.setText("1");
+        }else {
+
+        }
     }
 }
