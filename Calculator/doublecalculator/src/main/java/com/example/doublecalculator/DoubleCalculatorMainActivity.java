41bd9c2b7da49e880f6213b3655f94e50b053687
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-01 15:04:12 +0900

    6/01 모듈 추가(실수형 계산기) / UI 만들기

---
+++ Calculator/doublecalculator/src/main/java/com/example/doublecalculator/DoubleCalculatorMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.doublecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoubleCalculatorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_double_calculator_main);
+    }
+}
